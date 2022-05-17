e44274869c06dda9ab2958aaa666d3d912bc5c67
atuikhar <atuikhar@gmail.com>
2022-05-17 15:57:21 +0530

    ok

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import Theme from './styles/Theme';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './store';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import Theme from './styles/Theme'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './store'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
   <Provider store={store}>
     <BrowserRouter>
       <Theme>
         <App />
       </Theme>
     </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
-);
+  </Provider>
+)
